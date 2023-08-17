7a4b5a01be2c113391a42c3d2e1db9e15889307a
Min <sou05094@gmail.com>
2023-08-17 09:21:16 +0900

    8/17 수정

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load('ultralytics/yolov5','custom',path='best.pt')  # force_reload = recache latest code
+    model = torch.hub.load('ultralytics/yolov5','custom',path='./weights/best.pt')  # force_reload = recache latest code
     model.eval()
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
